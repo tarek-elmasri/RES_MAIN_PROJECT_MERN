cb4505a4d4c21a0c30efd510abb5e6790cfa02eb
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2021-04-12 15:47:18 +0300

    adding profile model

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,8 @@
 const usersController = require('./users_controller')
-
+const profileController = require('./profiles_controller')
 
 
 module.exports = {
-  usersController
+  usersController,
+  profileController
 }
